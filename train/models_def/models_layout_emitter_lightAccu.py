fixed not normalizing emitter_outdirs_emitter_local bug

train/models_def/models_layout_emitter_lightAccu.py
@@ -178,6 +178,7 @@ class emitter_lightAccu(nn.Module):
         self.vv_envmap, self.uu_envmap = torch.meshgrid(torch.arange(self.envRow), torch.arange(self.envCol))
         self.vv_envmap, self.uu_envmap = self.vv_envmap.cuda(), self.uu_envmap.cuda()
         self.rL = renderingLayer(imWidth = envCol, imHeight = envRow)
+        self.rL_dest = renderingLayer(imWidth = envCol, imHeight = envRow, envWidth = scatterWidth, envHeight = scatterHeight)
 
         if self.opt is not None:
             self.im_width, self.im_height = self.opt.cfg.DATA.im_width, self.opt.cfg.DATA.im_height
@@ -320,6 +321,7 @@ class emitter_lightAccu(nn.Module):
         emitter_global2localLightNet_trans_matrix = emitter_global2localLightNet_trans_matrix.unsqueeze(2).unsqueeze(2) # [2, 386, 1, 1, 3, 3]
 
         emitter_outdirs_emitter_local = (emitter_global2localLightNet_trans_matrix @ emitter_outdirs.unsqueeze(-1)).squeeze(-1) # [2, 384, 120, 160, 3]
+        emitter_outdirs_emitter_local = emitter_outdirs_emitter_local / (torch.linalg.norm(emitter_outdirs_emitter_local, axis=-1, keepdim=True)+1e-6)
 
         # l_local -> pixel coords in envmap
         cos_theta = emitter_outdirs_emitter_local[:, :, :, :, 2]
@@ -371,18 +373,8 @@ class emitter_lightAccu(nn.Module):
         '''
         return global directions of the scattered panorama (hemisphere pixels) at the emitters; should look like ![](https://i.imgur.com/sO8m431.jpg)
         '''
-        emitter_outdirs_meshgrid_emitter_local = self.rL.ls.reshape(self.scatterHeight, self.scatterWidth, 3).unsqueeze(-1).unsqueeze(0).unsqueeze(0)
+        emitter_outdirs_meshgrid_emitter_local = self.rL_dest.ls.reshape(self.scatterHeight, self.scatterWidth, 3).unsqueeze(-1).unsqueeze(0).unsqueeze(0)
 
-        # emitter_global2localLightNet_trans_matrix: torch.Size([2, 384, 1, 1, 3, 3])
-        # print(emitter_global2localLightNet_trans_matrix[0, 0, 0, 0, :, :])
-        # print(emitter_global2localLightNet_trans_matrix @ torch.transpose(emitter_global2localLightNet_trans_matrix, -1, -2))
-        # eyes = torch.eye(3).float().cuda().reshape(1, 1, 1, 1, 3, 3)
-        # emitter_outdirs_meshgrid = torch.inverse(emitter_global2localLightNet_trans_matrix + 1e-6*eyes) @ emitter_outdirs_meshgrid_emitter_local
-        # print(emitter_global2localLightNet_trans_matrix.shape, emitter_outdirs_meshgrid_emitter_local.shape, emitter_global2localLightNet_trans_matrix.dtype, emitter_outdirs_meshgrid_emitter_local.dtype, )
-        # a = emitter_global2localLightNet_trans_matrix
-        # b = emitter_global2localLightNet_trans_matrix.transpose(-1, -2)
-        # print(a)
-        # print(b)
         emitter_outdirs_meshgrid = emitter_global2localLightNet_trans_matrix.transpose(-1, -2) @ emitter_outdirs_meshgrid_emitter_local # emitter_global2localLightNet_trans_matrix is orthogonal; inv == transpose
         emitter_outdirs_meshgrid = emitter_outdirs_meshgrid.squeeze(-1) # [2, 384, 8, 16, 3]
         return emitter_outdirs_meshgrid

train/utils/utils_total3D/utils_OR_visualize.py
@@ -303,7 +303,9 @@ class Box(Scene3D):
 
         return depth_combined, mask_conflict
 
-    def draw_3D_scene_plt(self, type = 'prediction', if_save = True, save_path='', fig_or_ax=[None, None],  which_to_vis='cell_info', if_show_emitter=True, if_show_objs=True, if_return_cells_vis_info=False, hide_cells=False, hide_random_id=True, scale_emitter_length=1.):
+    def draw_3D_scene_plt(self, type = 'prediction', if_save = True, save_path='', fig_or_ax=[None, None],  which_to_vis='cell_info', \
+            if_show_emitter=True, if_show_objs=True, if_return_cells_vis_info=False, hide_cells=False, hide_random_id=True, scale_emitter_length=1., \
+            if_print_log=False):
         assert type in ['prediction', 'GT', 'both']
         figs_to_draw = {'prediction': ['prediction'], 'GT': ['GT'],'both': ['prediction', 'GT']}
         figs_to_draw = figs_to_draw[type]
@@ -512,6 +514,7 @@ class Box(Scene3D):
 
                 for cell_info in cell_info_grid:
                     wall_idx, i, j = cell_info['wallidx_i_j']
+
                     origin_v1_v2 = basis_v_indexes[wall_idx]
                     basis_1 = (layout[origin_v1_v2[1]] - layout[origin_v1_v2[0]]) / self.grid_size
                     basis_2 = (layout[origin_v1_v2[2]] - layout[origin_v1_v2[0]]) / self.grid_size
@@ -550,6 +553,12 @@ class Box(Scene3D):
                             verts = (np.array(verts).squeeze().T - cell_vis['extra_info']['cell_center']) * (cell_vis['alpha']/2.+0.5) + cell_vis['extra_info']['cell_center']
                             verts = [(verts.T).tolist()]
                             poly = Poly3DCollection(verts, facecolor=intensity_color, edgecolor=color)
+
+                            if if_print_log:
+                                if type0 == 'GT' and cell_info['obj_type'] == 'window':
+                                    # ic('------')
+                                    print(wall_idx, i, j)
+
                         else:
                             poly = Poly3DCollection(verts, facecolor=color)
 
@@ -616,7 +625,7 @@ class Box(Scene3D):
         else:
             return ax_3d, [ax_3d_GT, ax_3d_PRED, [cells_vis_info_list_pred, cells_vis_info_list_GT]]
 
-    def draw_all_cells(self, ax_3d, layout, lightnet_array_GT, alpha=0.5):
+    def draw_all_cells(self, ax_3d, layout, lightnet_array_GT, alpha=0.5, if_print_log=False, highlight_cells=[]):
         assert lightnet_array_GT.shape == (6, self.grid_size, self.grid_size, 3)
         basis_v_indexes = [(3, 2, 0), (7, 6, 4), (4, 5, 0), (6, 2, 5), (7, 6, 3), (7, 3, 4)]
 
@@ -636,7 +645,8 @@ class Box(Scene3D):
 
                     verts = np.array(verts).squeeze()
                     verts = [verts.tolist()]
-                    poly = Poly3DCollection(verts, facecolor=lightnet_array_GT[wall_idx, i, j, :].flatten().tolist(), edgecolor='k')
+                    if_highlight = (wall_idx, i, j) in highlight_cells
+                    poly = Poly3DCollection(verts, facecolor=lightnet_array_GT[wall_idx, i, j, :].flatten().tolist(), edgecolor='r' if if_highlight else 'k', linewidths=3 if if_highlight else 1)
 
                     cell_vis = {}
                     cell_vis.update({'poly': poly})