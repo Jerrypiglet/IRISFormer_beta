ad1eb041b658adb07b31603af9ab05b6cd2af4ae
jerrypiglet <jerrypiglet@gmail.com>
2021-09-28 01:18:30 -0700

    finished adding SG lighting vis to vis_per-pixel-light_20210922.ipynb; https://i.imgur.com/24QtrDE.jpg

--- train/utils/utils_io.py
+++ train/utils/utils_io.py
@@ -3,7 +3,41 @@ import numpy as np
 import struct
 import os.path as osp
 import cv2
+from skimage.measure import block_reduce
 
+def loadEnvmap(envName, envRow, envCol, envHeight, envWidth):
+    print('>>>>loadEnvmap', envName)
+    if not osp.isfile(envName ):
+        env = np.zeros( [3, envRow, envCol,
+            envHeight, envWidth], dtype = np.float32 )
+        envInd = np.zeros([1, 1, 1], dtype=np.float32 )
+        print('Warning: the envmap %s does not exist.' % envName )
+        return env, envInd
+    else:
+        envHeightOrig, envWidthOrig = 16, 32
+        assert( (envHeightOrig / envHeight) == (envWidthOrig / envWidth) )
+        assert( envHeightOrig % envHeight == 0)
+
+        env = cv2.imread(envName, -1 ) 
+        print(env.shape)
+
+        if not env is None:
+            env = env.reshape(envRow, envHeightOrig, envCol,
+                envWidthOrig, 3) # (1920, 5120, 3) -> (120, 16, 160, 32, 3)
+            env = np.ascontiguousarray(env.transpose([4, 0, 2, 1, 3] ) ) # -> (3, 120, 160, 16, 32)
+
+            scale = envHeightOrig / envHeight
+            if scale > 1:
+                env = block_reduce(env, block_size = (1, 1, 1, 2, 2), func = np.mean )
+
+            envInd = np.ones([1, 1, 1], dtype=np.float32 )
+            return env, envInd
+        else:
+            env = np.zeros( [3, envRow, envCol,
+                envHeight, envWidth], dtype = np.float32 )
+            envInd = np.zeros([1, 1, 1], dtype=np.float32 )
+            print('Warning: the envmap %s does not exist.' % envName )
+            return env, envInd
 def loadH5(imName ): 
     try:
         hf = h5py.File(imName, 'r')
@@ -70,3 +104,18 @@ def loadImage(imName=None, im=None, isGama = False, resize_HW=[-1, -1]):
 
     return im
 
+def loadHdr(imName, im_width=None, im_height=None):
+    if not(osp.isfile(imName ) ):
+#         print(imName )
+        assert(False )
+    im = cv2.imread(imName, -1)
+    # print(imName, im.shape, im.dtype)
+
+    if im is None:
+        print(imName )
+        assert(False )
+    if im_width is not None and im_height is not None:
+        im = cv2.resize(im, (im_width, im_height), interpolation = cv2.INTER_AREA )
+    im = np.transpose(im, [2, 0, 1])
+    im = im[::-1, :, :]
+    return im.transpose(1, 2, 0)
