ed196dccdf4adc4578510146ffb96650a59c0ed4
jerrypiglet <jerrypiglet@gmail.com>
2023-05-03 17:42:24 -0700

    0

--- notebooks/create_mini_dataset.py
+++
@@ -1,4 +0,0 @@
-import shutil  
-
-scene_list = ['scene0001_01', 'scene0196_00', 'scene0540_00']
-split_list = ['mainDiffLight_xml', 'mainDiffLight_xml1', 'mainDiffMat_xml', 'mainDiffMat_xml1', 'main_xml', 'main_xml1']
